929069b842c0900400fec168084e649a5333f64f
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-06-01 18:17:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -13,32 +14,30 @@ public class Main extends Application {
 
     private TextField directoryPathField;
     private TextField searchField;
+    private TextArea resultArea;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("File Browser and Search");
 
-
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
+        
         directoryPathField = new TextField();
         directoryPathField.setPromptText("Enter directory path");
-
-
+        
         searchField = new TextField();
         searchField.setPromptText("Enter search phrase");
-
-
+        
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory());
-
-
+        
         Button searchButton = new Button("Search");
-
-
+        
         HBox hBox = new HBox(10, directoryPathField, browseButton);
-        VBox vBox = new VBox(10, hBox, searchField, searchButton);
-
-
-        Scene scene = new Scene(vBox, 600, 200);
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
+        
+        Scene scene = new Scene(vBox, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
