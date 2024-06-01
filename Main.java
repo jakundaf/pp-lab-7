7d48586b1571584e1ab13fa05a7c1ecc595c6bda
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-06-01 18:24:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,23 +20,24 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("File Browser and Search");
 
-        resultArea = new TextArea();
-        resultArea.setPrefHeight(400);
-        
         directoryPathField = new TextField();
         directoryPathField.setPromptText("Enter directory path");
-        
+
         searchField = new TextField();
         searchField.setPromptText("Enter search phrase");
-        
+
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
+
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory());
-        
+
         Button searchButton = new Button("Search");
-        
+        searchButton.setOnAction(e -> searchFiles());
+
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
-        
+
         Scene scene = new Scene(vBox, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -48,6 +49,34 @@ public class Main extends Application {
 
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    private void searchFiles() {
+        if (directoryPathField.getText().isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPathField.getText());
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    results.append(file.getAbsolutePath()).append("\n");
+                }
+            }
         }
     }
 
